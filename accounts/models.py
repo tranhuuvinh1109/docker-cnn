b55358d4a3239b9e8d718a3f811799b7d5c89cc3
taed13 <taedtech13@gmail.com>
2023-10-02 15:15:24 +0700

    fixing

--- accounts/models.py
+++ accounts/models.py
@@ -1,53 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 import uuid
 from .manager import UserManager
 
 
 class User(AbstractUser):
-    username = None
-    email = models.EmailField( unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(
+        max_length=254)
     is_verified = models.BooleanField(default=False)
+    avatar = models.CharField(max_length=255)
     otp = models.CharField(max_length=6, null=True, blank=True)
-    
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-    
-    objects = UserManager()
-    
-    def name(self):
-        return self.first_name + ' ' + self.last_name
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    progress = models.IntegerField()
+    status = models.CharField(max_length=255)
+    link_drive = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.email
+        return f"Project {self.id} - {self.status}"
 
 
 class ForgetPassword(models.Model):
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
-    forget_password_token = models.CharField(max_length=200 ,null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    forget_password_token = models.CharField(
+        max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.user.email
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''  ALL SIGNALS HERE  '''
-
