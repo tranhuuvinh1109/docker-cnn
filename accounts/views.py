5dc171567e2e908f229ce87a5bdc545827084b3a
kkoahis <khoakun27@gmail.com>
2023-10-11 17:45:08 +0700

    api detect done

--- accounts/views.py
+++ accounts/views.py
@@ -4,44 +4,38 @@ from rest_framework.response import Response
 from django.http import JsonResponse
 import json
 from rest_framework.authtoken.models import Token
-
 from .serializers import *
 from .emails import *
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import ssl
 from django.shortcuts import get_object_or_404
 from export import views as export_views
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from io import BytesIO
+import os
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import tensorflow as tf
+import numpy as np
+from PIL import Image
 
 # Vô hiệu hóa kiểm tra chứng chỉ SSL
 ssl._create_default_https_context = ssl._create_unverified_context
-
-# Create your views here.
-
-
 class UserSerializerNested(serializers.Serializer):
     id = serializers.IntegerField()
     email = serializers.EmailField()
     avatar = serializers.CharField()
     username = serializers.CharField()
-
-
 class ProjectSerializer(serializers.ModelSerializer):
     user = UserSerializerNested()
-
     class Meta:
         model = Project
         fields = ['id', 'user', 'progress', 'status', 'link_drive']
-
-
 class RegisterAPI(APIView):
     def post(self, request):
-        print("->>>>>>>>>>>>>>>>>>>>>>>", request.data)
         data = request.data
         serializers = UserSerializer(data=data)
-
         if serializers.is_valid():
             serializers.save()
             send_otp_via_email(serializers.data['email'])
@@ -56,18 +50,14 @@ class RegisterAPI(APIView):
             'message': 'User registration failed, please try again',
             'data': serializers.errors
         })
-
-
 class VerifyOTP(APIView):
     def post(self, request):
         try:
             data = request.data
             serializers = VerifyAccountSerializer(data=data)
-
             if serializers.is_valid():
                 email = serializers.data['email']
                 otp = serializers.data['otp']
-
                 user = User.objects.filter(email=email)
                 if not user.exists():
                     return Response({
@@ -105,15 +95,10 @@ class VerifyOTP(APIView):
         except Exception as e:
             return JsonResponse(json.dumps(e))
 
-
 class LoginAPI(APIView):
     def post(self, request):
-        """
-        Login API then response with user data and token
-        """
         try:
             serializers = LoginSerializer(data=request.data)
-
             if serializers.is_valid():
                 email = serializers.validated_data['email']
                 password = serializers.validated_data['password']
@@ -145,25 +130,20 @@ class LoginAPI(APIView):
                     })
         except serializers.ValidationError:
             print(serializers.ValidationError)
-
-
 class CreateProjectAPI(APIView):
     def post(self, request):
-        # Lấy dữ liệu đầu vào từ request.data
         user_id = request.data.get('user_id')
         progress = request.data.get('progress')
         status_text = request.data.get('status')
         link_drive = request.data.get('link_drive')
         file = request.FILES.get("file")
 
-        # Tìm người dùng dựa trên user_id
         try:
             user = User.objects.get(id=user_id)
             print(">>>user -->:", user)
         except User.DoesNotExist:
             return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
         
-        # Tạo dự án mới với người dùng tìm được
         project = Project.objects.create(
             user=user, progress=progress, status=status_text, link_drive=link_drive)
         print("Project created -->0", project)
@@ -188,17 +168,9 @@ class InforUser(APIView):
             user = User.objects.get(id=user_id)
         except User.DoesNotExist:
             return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        # Lấy danh sách các project của user
         projects = Project.objects.filter(user=user)
-
-        # Serialize danh sách các project
         serializer = ProjectSerializer(projects, many=True)
-
-        # Serialize thông tin người dùng
         user_data = UserSerializerNested(user).data
-
-        # Tạo response chứa thông tin đã được serialize
         response_data = {
             'message': f'Information for user with id {user_id}',
             'data': {
@@ -206,96 +178,120 @@ class InforUser(APIView):
                 'projects': serializer.data
             }
         }
-
         return Response(response_data, status=status.HTTP_200_OK)
 
 
 class UpdateProjectStatus(APIView):
     def post(self, request, project_id):
         try:
-            # Lấy thông tin project với project_id
             project = Project.objects.get(id=project_id)
         except Project.DoesNotExist:
             return Response({"message": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        # Lấy dữ liệu mới từ yêu cầu PUT
         new_status = request.data.get('status', None)
-
         if new_status is None:
             return Response({"message": "Status is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Cập nhật trạng thái
         project.status = new_status
         project.save()
 
-        # Serialize thông tin đã cập nhật
         serializer = ProjectSerializer(project)
-
         response_data = {
             'message': 'Project status updated successfully',
             'data': serializer.data
         }
-
         return Response(response_data, status=status.HTTP_200_OK)
-
 
 class UpdateProject(APIView):
     def put(self, request):
-        # Lấy dữ liệu từ yêu cầu PUT
         project_data = request.data
-
         try:
-            # Lấy thông tin project với project_id
             project_id = project_data.get('id')
             project = Project.objects.get(id=project_id)
         except Project.DoesNotExist:
             return Response({"message": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        # Cập nhật thông tin project
         project.status = project_data.get('status', project.status)
         project.link_drive = project_data.get('linkDrive', project.link_drive)
         project.save()
 
-        # Serialize thông tin đã cập nhật
         serializer = ProjectSerializer(project)
-
         response_data = {
             'message': 'Project updated successfully',
             'data': serializer.data
         }
-
         return Response(response_data, status=status.HTTP_200_OK)
 
 
 class UserDataManageAPI(APIView):
     def get(self, request):
-        # Ottieni tutti gli utenti
         users = User.objects.all()
-
-        # Creare una lista per memorizzare i dati dell'utente e i relativi progetti
         data_list = []
 
         for user in users:
-            # Serializza le informazioni sull'utente
             user_serializer = UserSerializer(user)
 
-            # Ottieni i progetti associati all'utente corrente
             projects = Project.objects.filter(user=user)
             project_serializer = ProjectSerializer(projects, many=True)
-
-            # Crea il dato dell'utente e i relativi progetti
             user_data = {
                 "user": user_serializer.data,
                 "projects": project_serializer.data
             }
-
-            # Aggiungi il dato dell'utente alla lista
             data_list.append(user_data)
-
-        # Creare la risposta JSON
         response_data = {
             "message": "Get data user manage project successful",
             "data": data_list
         }
 
         return Response(response_data, status=status.HTTP_200_OK)
+
+
+model_path = 'D:/Django/CNN/docker-cnn/model/bike_car.h5'
+model = tf.keras.models.load_model(model_path)
+class DetectImage(APIView):
+    def post(self, request):
+        # Kiểm tra xem có file hình ảnh được gửi lên không
+        if 'image' not in request.FILES:
+            return Response({'message': 'No image uploaded'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Đọc file ảnh từ request.FILES
+        image_file = request.FILES['image']
+
+        # Đọc ảnh từ buffer
+        img = Image.open(image_file)
+        img = img.convert('RGB')  # Chuyển đổi thành ảnh màu nếu không phải
+
+        # Thực hiện nhận diện trên ảnh
+        predicted_label, confidence = self.detect_label(img)
+
+        # Chuyển đổi độ tin cậy thành phần trăm
+        confidence_percentage = confidence * 100
+
+        # Chuẩn bị phản hồi
+        response_data = {
+            'message': 'Detection successful',
+            'predicted_label': predicted_label,
+            # Hiển thị độ tin cậy dưới dạng phần trăm
+            'confidence': f'{confidence_percentage:.2f}%'
+
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+    def detect_label(self, img):
+        # Tiền xử lý ảnh
+        # Thay đổi kích thước ảnh theo model của bạn
+        img = img.resize((128, 128))
+        img = np.array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+
+        # Dự đoán
+        predictions = model.predict(img)
+        predicted_class_index = np.argmax(predictions[0])
+        class_labels = ['Bike', 'Car']  # Thay bằng danh sách nhãn của bạn
+        predicted_class = class_labels[predicted_class_index]
+
+        # Lấy confidence (độ tin cậy)
+        confidence = predictions[0][predicted_class_index]
+
+        return predicted_class, confidence
